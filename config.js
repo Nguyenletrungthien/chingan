504233e21fd83a97e7cc969c714046a5b5220376
Nguyenletrungthien <101155715+Nguyenletrungthien@users.noreply.github.com>
2022-10-18 18:43:04 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+const nameGirl = 'chị';
+const giftUrl = 'https://www.facebook.com/boo.pe.52';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng chị của em';
+const contentCard = 'Chị à! Nhân ngày 20/10, em xin chúc chị mãi mãi khỏe mạnh, hành phúc và thành công trên con đường chị đã và đang đi. Chúc gia đình chị trăm năm hạnh phúc như bây giờ, chị nhé! ';
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
